0f5230f8f51ec4fa9a46178b3596bade5623e9aa
Martin Pač <martinpac87@gmail.com>
2025-02-14 09:35:53 +0100

    Adde option for subdirectories and to delete original files

--- subtitles-ass-to-srt-converter/README.md
+++ subtitles-ass-to-srt-converter/README.md
@@ -1,5 +1,8 @@
 # .ass to .srt converter
 Skript slúži na konvertovanie všetkých .ass súboroch v zadanom priečinku na .srt.
+Nastaviteľné možnosti:
+- rekurzívne = konvertovať tiež vo všetkých podpriečinkoch
+- vymazať pôvodne súbory (.ass)
 Využité knižnice:
 - sys
 - os: listdir,isfile,join
@@ -8,6 +11,9 @@ Využité knižnice:
 
 # .ass to .srt converter
 Script is converting all .ass files located in selected dir to .srt files.
+Custom options:
+- recursive = convert also in all subdirectories
+- delete original files (.ass)
 Used libraries:
 - sys
 - os: listdir,isfile,join
--- subtitles-ass-to-srt-converter/ass_to_srt_converter.py
+++ subtitles-ass-to-srt-converter/ass_to_srt_converter.py
@@ -17,11 +17,11 @@ except ImportError:
 
 master = Tk()
 
-def convert_to_srt():
-    dir=E1.get()
-    files = [f for f in listdir(dir) if isfile(join(dir, f)) & f.endswith('.ass')] 
+def recursive_convert_to_srt(dir,recursive=1,delOrigin=1): 
     pocet=0
+    files = [f for f in listdir(dir) if isfile(join(dir, f)) & f.endswith('.ass')]
     for fileName in files:
+        fileName=join(dir, fileName)
         with open(fileName, 'r+', encoding="utf8") as file:
             srt_lines = asstosrt.convert(file)
             with open(fileName.replace(".ass",".srt"), 'w', encoding="utf8",newline='') as file_srt:
@@ -29,14 +29,34 @@ def convert_to_srt():
                 pocet+=1
                 file_srt.close()
             file.close()
+            if (delOrigin==1):
+                os.remove(fileName)
+    if (recursive==1):
+        dirs = [f for f in os.listdir(dir) if not isfile(join(dir, f))]
+        if dirs:
+            for xdir in dirs:
+                pocet+=recursive_convert_to_srt(join(dir, xdir),recursive,delOrigin)
+    return pocet    
+
+    
+def convert_to_srt():
+    dir=E1.get()
+    subdir=var1.get()
+    delOrigin=var2.get()
+    pocet=0
+    pocet+=recursive_convert_to_srt(dir,subdir,delOrigin)
     message.config(text="Converted "+str(pocet)+" files")        
-master.geometry("300x160")        
+master.geometry("270x160")        
 Label(master, text="Zadaj priečinok:",fg="red").grid(row=0, sticky=W,column=0)
 E1 = Entry(master, bd =5,width=40)
 E1.grid(row=1,column=0)
 E1.insert(0,"./")
+var1 = IntVar()
+Checkbutton(master, text="Sub-directories", variable=var1).grid(row=2, sticky=W, column=0)
+var2 = IntVar()
+Checkbutton(master, text="Delete .ass", variable=var2).grid(row=3, sticky=W, column=0)
 message=Label(master, text="",fg="green")
-message.grid(row=2,column=0)
-Button(master, text='Convert to .srt', command=convert_to_srt,fg="blue").grid(row=3,column=0)
+message.grid(row=4,column=0)
+Button(master, text='Convert to .srt', command=convert_to_srt,fg="blue").grid(row=5,column=0)
 mainloop()            
             
